e2818620cebb58c8548c484adc887365ec05f958
zhul <zhul@iriugd.com>
2023-09-05 17:56:38 +0800

    updata log.py

--- log.py
+++ log.py
@@ -1,4 +1,5 @@
 import logging
+import os
 import time
 
 
@@ -8,6 +9,16 @@ class CustomFormatter(logging.Formatter):
         return str(int(time.time()))
 
 def log_init():
+
+    log_file = 'upgrade.log'
+    # 清空日志文件内容
+    # with open(log_file, 'w'):
+    #     pass
+
+    if os.path.exists(log_file):
+        os.remove(log_file)
+
+
     logging.basicConfig(filename='upgrade.log', level=logging.INFO, format='%(asctime)s#%(code)s#%(message)s##', datefmt='%s')
     logger = logging.getLogger()
     logger.handlers[0].setFormatter(CustomFormatter('%(asctime)s#%(code)s#%(message)s##'))
